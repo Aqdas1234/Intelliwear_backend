d8fa1a3f2aee14da2153722e81b0433760f3b19f
MUHAMMADSAADNAWAZ <bsef21m549@pucit.edu.pk>
2025-03-01 11:02:25 +0500

    added swagger integration in intelliwear project apis , admin apis and some customer api

--- intelliwear/intelliwear/settings.py
+++ intelliwear/intelliwear/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'customerApi',
     'adminApi',
+    'drf_yasg',
     
 ]
 
--- intelliwear/intelliwear/urls.py
+++ intelliwear/intelliwear/urls.py
@@ -1,6 +1,20 @@
 from .views import RegisterView,LoginView,LogoutView,ChangePasswordView,PasswordResetConfirmView,PasswordResetRequestView
 from django.contrib import admin
 from django.urls import path,include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = "Intelliwear Api",
+        default_version= 'v1',
+        description= "Api Documentation of Intelliwear",
+    
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +29,7 @@ urlpatterns = [
     path('adminApi/', include('adminApi.urls')),
     path('customer/', include('customerApi.urls')),
     path('api-auth/', include('rest_framework.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+
 ]
