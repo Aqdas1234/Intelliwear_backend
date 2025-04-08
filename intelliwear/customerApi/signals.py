deab040b6c5c8b502681ec041dcb043357aee9c1
Aqdas Ikram <bsef21m505@pucit.edu.pk>
2025-04-08 12:35:57 +0500

    recommendation table will be updated when cart item is added

--- intelliwear/customerApi/signals.py
+++ intelliwear/customerApi/signals.py
@@ -17,6 +17,13 @@ def cart_item_added(sender, instance, created, **kwargs):
             cf_model = get_cf_model()
             if cf_model is not None:
                 cf_model.add_interaction(buyer_id=user.id, product_id=product_id, weight=weight)
+                recommended_ids = cf_model.get_recommendations(user_id=user.id, num_recommendations=30)
+                for pid in recommended_ids:
+                    try:
+                        product = Product.objects.get(id=pid)
+                        Recommendation.objects.get_or_create(user=user, product=product)
+                    except Product.DoesNotExist:
+                        print(f"Warning: Product with ID {pid} does not exist. Skipping.")
             else:
                 print("Error: Collaborative filtering model (cf_model) is None")
         else:
